6bf2ba8796267fd9e6a11bd321cbdc7726ee7fa8
Brian <brianchanyp@gmail.com>
2023-07-19 20:08:28 +1200

    Key for mapping

--- src/App.js
+++ src/App.js
@@ -9,15 +9,19 @@ class App extends Component {
 			monsters: [
 				{
 					name: "Frankenstein",
+					id: "asc1",
 				},
 				{
 					name: "Dracula",
+					id: "asc2",
 				},
 				{
 					name: "Zombie",
+					id: "asc3",
 				},
 				{
 					name: "Mummy",
+					id: "asc4",
 				},
 			],
 		};
@@ -26,7 +30,11 @@ class App extends Component {
 		return (
 			<div className="App">
 				{this.state.monsters.map((monster) => {
-					return <h1>{monster.name}</h1>;
+					return (
+						<div key={monster.id}>
+							<h1>{monster.name}</h1>
+						</div>
+					);
 				})}
 			</div>
 		);
