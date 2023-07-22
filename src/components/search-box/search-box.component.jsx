e7f6ef6bd388fd65caf1c31f5e6459beac2527da
Brian <brianchanyp@gmail.com>
2023-07-22 23:02:49 +1200

    Add css to search box and app.js background

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,11 +1,12 @@
 import { Component } from "react";
+import './search-box.component.css';
 
 class SearchBox extends Component {
   render() {
     return (
         
         <input
-        className={this.props.className} //css class
+        className={`search-box ${this.props.className}`} //css class
         type="search" // input type
         placeholder={this.props.placeholder} //placeholder
         onChange={this.props.onChangeHandler} //on change event
