d8b9fb6006761a9029331aab68e1d06ac41643a7
Brian <brianchanyp@gmail.com>
2023-07-26 21:46:39 +1200

    Change components from class to functional

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,23 +1,14 @@
-import{Component} from 'react';//importing React and Component from react
 import Card from '../card/card.component';//importing Card component
 
 import './card-list.styles.css';//importing card-list.styles.css file
 
 
-class CardList extends Component{//creating a class CardList which extends Component
-    render(){//render method
-        const{monsters}=this.props;//destructuring monsters from this.props
-        return(//returning the following
-            <div className='card-list'>
-               {monsters.map((monster)=>{//mapping through monsters
-               
-               return(//returning the following
-                     <Card monster={monster}/>//passing monster as props to Card component
-                );
-                })}
-            </div>
-        );
-    }
-}
+const CardList = ({monsters}) => (
+        <div className='card-list'>
+            {monsters.map(monster => {
+                return <Card monsters={monster}/>
+            })}
+        </div>
+        );//creating CardList component and passing monsters as props and mapping through monsters array and passing each monster as props to Card component
 
 export default CardList;//  exporting CardList component
